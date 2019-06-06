8c7fa5edfe70a7518f7bdc95ee81fba85652e05c
ludoool <36404534+ludoool@users.noreply.github.com>
2019-06-06 13:12:05 +0200

    Add files via upload

---
+++ mysql/src/Liaison.java
@@ -0,0 +1,40 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author liamsi
+ */
+public class Liaison {
+    Connection con;
+public Liaison(){
+    try {
+Class.forName("com.mysql.jdbc.Driver");
+System.out.println("La base de donnée est chargée");
+} 
+catch(ClassNotFoundException e)
+{
+System.out.println(e);
+}
+
+try {
+con=DriverManager.getConnection("jdbc:mysql://localhost/commande","root","");// a la place de commande on met notre bdd
+}
+catch(SQLException e){System.out.println(e);}
+
+}
+Connection ObtenirConnextion()
+{
+    return con;
+}
+
+
+}
